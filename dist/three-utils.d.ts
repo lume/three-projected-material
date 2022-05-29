efb450c7d51b9f50cfa25f85be6d1aa19d62604c
Joe Pea <joe@trusktr.io>
2022-05-28 19:26:54 -0700

    remove build output from the repo, add a prepare script so that the lib is still installable from git

--- dist/three-utils.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Texture } from 'three';
-export declare function monkeyPatch(shader: string, { defines, header, main, ...replacements }: {
-    [x: string]: any;
-    defines?: Record<string, string> | undefined;
-    header?: string | undefined;
-    main?: string | undefined;
-}): string;
-export declare function addLoadListener(texture: Texture, callback: (t: Texture) => void): void;
